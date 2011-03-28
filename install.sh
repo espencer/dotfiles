e6c5c66355f24092b129fae9e49d6c8e94d422ee
David Lopez <david@wopr.(none)>
2011-03-28 17:58:15 +0200

    rename files: deleted starting dot at filenames

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 DIRECTORY=$(dirname $(readlink -f $0))
 echo Installing .vimrc and .screenrc files to $DIRECTORY links...
 
-ln -s "$DIRECTORY/.vimrc" "$HOME/.vimrc"
-ln -s "$DIRECTORY/.screenrc" "$HOME/.screenrc"
-ln -s "$DIRECTORY/.gvimrc" "$HOME/.gvimrc"
+ln -s "$DIRECTORY/vimrc" "$HOME/.vimrc"
+ln -s "$DIRECTORY/screenrc" "$HOME/.screenrc"
+ln -s "$DIRECTORY/gvimrc" "$HOME/.gvimrc"
+ln -s "$DIRECTORY/editrc" "$HOME/.editrc"
+ln -s "$DIRECTORY/inputrc" "$HOME/.inputrc"
